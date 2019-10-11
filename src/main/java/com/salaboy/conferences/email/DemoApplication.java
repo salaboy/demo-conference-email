23240d3bee90316255d2d991a01793c81893ed4c
salaboy <salaboy@gmail.com>
2019-10-11 14:18:36 +0100

    adding zeebe worker

--- src/main/java/com/salaboy/conferences/email/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/email/DemoApplication.java
@@ -1,6 +1,10 @@
 package com.salaboy.conferences.email;
 
 import com.salaboy.conferences.email.model.Proposal;
+import io.zeebe.client.api.response.ActivatedJob;
+import io.zeebe.client.api.worker.JobClient;
+import io.zeebe.spring.client.EnableZeebeClient;
+import io.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +14,7 @@ import java.util.*;
 
 @SpringBootApplication
 @RestController
-
+@EnableZeebeClient
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -28,6 +32,11 @@ public class DemoApplication {
     @PostMapping()
     public void sendEmailNotification(@RequestBody Proposal proposal) {
         System.out.println("Email Sent for proposal: " + proposal);
+    }
+
+    @ZeebeWorker(type = "email")
+    public void sendEmailNotification(final JobClient client, final ActivatedJob job) {
+        sendEmailNotification((Proposal) job.getVariablesAsMap().get("proposal"));
     }
 
 
